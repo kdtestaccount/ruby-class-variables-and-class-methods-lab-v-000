b40b98329b169f8c06e5a418daf4d8db7b8e81c2
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-12 03:41:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,7 @@ class Song
   def self.genre_count
     GENRES.each do |variable|
       genre_count_int = GENRES.count(variable)
-      push "GENRE COUNT: #{genre_count_int}"
+      puts "GENRE COUNT: #{genre_count_int}"
       GENRE_COUNT.store(variable, genre_count_int)
     end
     GENRE_COUNT
